ba5900165445685fbc103dad367775c0a612f731
Alex <alex.nektony@gmail.com>
2021-05-09 17:04:43 +0300

    initial project state

---
+++ ReadersWriters/ReadersWriters/ReadersWriters/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ReadersWriters
+//
+//  Created by Alex on 5/9/21.
+//  Copyright © 2021 AlexCo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
+
